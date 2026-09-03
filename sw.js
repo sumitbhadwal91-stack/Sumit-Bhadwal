5fecac01c1378a1e8ca6ff7f9fe7e6bb1aa66c8f
sumitbhadwal91-stack <sumitbhadwal91@gmail.com>
2026-09-04 02:34:15 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='jct-rate-master-cloud-v6';
+const C='jct-rate-master-cloud-v7';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./firebase-config.js','./icons/sb-bala-192.png','./icons/sb-bala-512.png']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
